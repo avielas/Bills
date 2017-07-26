cb3b7fcfb787ba14980cfeb108bd564aa8575905
Avraham, Aviel <avielas@gmail.com>
2017-07-26 21:28:20 +0300

    alpha 0.02

--- Bills/openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ Bills/openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/aviel/AndroidStudioProjects/bills/Bills/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/aviel/AndroidStudioProjects/bills/bills/Bills/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
