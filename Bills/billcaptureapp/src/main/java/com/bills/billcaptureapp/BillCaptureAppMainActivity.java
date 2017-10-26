cfebfbcfae6c1d2e069269b9c8befeb25a4c3783
Avraham, Aviel <avielas@gmail.com>
2017-10-27 00:00:18 +0300

    added and connect fragments

--- Bills/billcaptureapp/src/main/java/com/bills/billcaptureapp/BillCaptureAppMainActivity.java
+++ Bills/billcaptureapp/src/main/java/com/bills/billcaptureapp/BillCaptureAppMainActivity.java
@@ -1,11 +1,9 @@
 package com.bills.billcaptureapp;
 
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.widget.Button;
-
+import com.bills.billcaptureapp.fragments.WelcomeScreenFragment;
 import com.bills.billslib.Contracts.BillRow;
 import com.bills.billslib.Contracts.Enums.LogLevel;
 import com.bills.billslib.Core.BillsLog;
@@ -13,107 +11,27 @@ import com.bills.billslib.Core.MainActivityBase;
 import com.bills.billslib.Utilities.FilesHandler;
 import com.bills.testslib.CameraFragment;
 import com.bills.testslib.TestsUtilities;
-
 import java.util.List;
 
-public class BillCaptureAppMainActivity extends MainActivityBase implements com.bills.billslib.Fragments.CameraFragment.OnFragmentInteractionListener {
+public class BillCaptureAppMainActivity extends MainActivityBase implements
+        com.bills.billslib.Fragments.CameraFragment.OnFragmentInteractionListener,
+        com.bills.billcaptureapp.fragments.WelcomeScreenFragment.OnFragmentInteractionListener{
     private String Tag = BillCaptureAppMainActivity.class.getName();
-    private final int REQUEST_CODE = 1;
-    Button _clickToSimpleCapture;
-    Button _clickToRightCapture;
-    Button _clickToLeftCapture;
-    Button _clickToRemotlyCapture;
-    Button _clickToStraightCapture;
-    final Handler mHandler = new Handler();
-    private String mResult;
     private CameraFragment mCameraFragment;
+    private WelcomeScreenFragment mWelcomScreenFragment;
+    private Fragment mCurrentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_capture_app_main);
         TestsUtilities.InitBillsLogToLogcat();
-
-        //Initialize parameters
-        _clickToSimpleCapture = findViewById(R.id.simpleCaptureButton);
-        _clickToRightCapture = findViewById(R.id.rightCaptureButton);
-        _clickToLeftCapture = findViewById(R.id.leftCaptureButton);
-        _clickToRemotlyCapture = findViewById(R.id.remotlyCaptureButton);
-        _clickToStraightCapture = findViewById(R.id.straightCaptureButton);
-        AddListenerToSimpleCaptureButton();
-        AddListenerToRightCaptureButton();
-        AddListenerToLeftCaptureButton();
-        AddListenerToRemotlyCaptureButton();
-        AddListenerToStraightCaptureButton();
         mCameraFragment = new CameraFragment();
+        mWelcomScreenFragment = new WelcomeScreenFragment();
+        StartWelcomeFragment(null);
     }
 
-    public void AddListenerToSimpleCaptureButton() {
-        _clickToSimpleCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                try {
-                    startTestThread();
-                } catch (Exception e) {
-                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
-                }
-            }
-        });
-    }
-
-    public void AddListenerToRightCaptureButton() {
-        _clickToRightCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                try {
-
-                } catch (Exception e) {
-                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
-                }
-            }
-        });
-    }
-
-    public void AddListenerToLeftCaptureButton() {
-        _clickToLeftCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                try {
-
-                } catch (Exception e) {
-                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
-                }
-            }
-        });
-    }
-
-    public void AddListenerToRemotlyCaptureButton() {
-        _clickToRemotlyCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                try {
-
-                } catch (Exception e) {
-                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
-                }
-            }
-        });
-    }
-
-    public void AddListenerToStraightCaptureButton() {
-        _clickToStraightCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                try {
-
-                } catch (Exception e) {
-                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
-                }
-            }
-        });
-    }
-
-    private void StartCameraFragment(int requestCode) {
+    public void StartCameraFragment() {
         try {
             mCameraFragment.Init(0, "");
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -122,33 +40,10 @@ public class BillCaptureAppMainActivity extends MainActivityBase implements com.
 
             // Commit the transaction
             transaction.commit();
-
-//            Intent intent = new Intent(getBaseContext(), CameraActivity.class);
-//            if (intent.resolveActivity(getPackageManager()) != null) {
-//                startActivityForResult(intent, requestCode);
-//            }
+            mCurrentFragment = mCameraFragment;
         } catch (Exception e) {
             BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
         }
-    }
-
-    // Create runnable for posting
-    final Runnable mUpdateResults = new Runnable() {
-        public void run() {
-            BillsLog.Log("Inchoo tutorial", LogLevel.Info, mResult);
-        }
-    };
-
-    protected void startTestThread() {
-        Thread t = new Thread() {
-            public void run() {
-                BillsLog.Log("Inchoo tutorial", LogLevel.Info, "My thread is running");
-                StartCameraFragment(REQUEST_CODE);
-//                mResult = "This is my new result";
-//                mHandler.post(mUpdateResults);
-            }
-        };
-        t.start();
     }
 
     @Override
@@ -157,8 +52,17 @@ public class BillCaptureAppMainActivity extends MainActivityBase implements com.
     }
 
     @Override
+    public void onBackPressed(){
+        if(mCurrentFragment == mCameraFragment || mCurrentFragment == mWelcomScreenFragment){
+            StartWelcomeFragment();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public void StartWelcomeFragment() {
-        throw new UnsupportedOperationException(Tag + ": StartWelcomeFragment");
+        StartWelcomeFragment(null);
     }
 
     @Override
@@ -167,28 +71,29 @@ public class BillCaptureAppMainActivity extends MainActivityBase implements com.
     }
 
     @Override
-    public void StartWelcomeFragment(byte[] image){
+    public void StartWelcomeFragment(final byte[] image) {
         try {
-            FilesHandler.toDelete(image);
-            setContentView(R.layout.activity_bill_capture_app_main);
-            TestsUtilities.InitBillsLogToLogcat();
-//
-//            //Initialize parameters
-//            _clickToSimpleCapture = findViewById(R.id.simpleCaptureButton);
-//            _clickToRightCapture = findViewById(R.id.rightCaptureButton);
-//            _clickToLeftCapture = findViewById(R.id.leftCaptureButton);
-//            _clickToRemotlyCapture = findViewById(R.id.remotlyCaptureButton);
-//            _clickToStraightCapture = findViewById(R.id.straightCaptureButton);
-//            AddListenerToSimpleCaptureButton();
-//            AddListenerToRightCaptureButton();
-//            AddListenerToLeftCaptureButton();
-//            AddListenerToRemotlyCaptureButton();
-//            AddListenerToStraightCaptureButton();
-//            mCameraFragment = new CameraFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, mWelcomScreenFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+            mCurrentFragment = mWelcomScreenFragment;
+            if (null != image) {
+                Thread t = new Thread() {
+                public void run() {
+                    try {
+                        FilesHandler.toDelete(image);
+                    } catch (Exception e) {
+                        BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
+                    }
+                }
+                };
+                t.start();
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
         }
-//        BillsLog.Log("Inchoo tutorial", LogLevel.Info, lastCapturedBillPath);
-//        throw new UnsupportedOperationException(Tag + ": Finish");
     }
 }
---
+++ Bills/billcaptureapp/src/main/java/com/bills/billcaptureapp/fragments/WelcomeScreenFragment.java
@@ -0,0 +1,164 @@
+package com.bills.billcaptureapp.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.bills.billcaptureapp.R;
+import com.bills.billslib.Contracts.Enums.LogLevel;
+import com.bills.billslib.Core.BillsLog;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WelcomeScreenFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class WelcomeScreenFragment extends Fragment implements View.OnClickListener{
+    private String Tag = WelcomeScreenFragment.class.getName();
+    Button _clickToSimpleCapture;
+    Button _clickToRightCapture;
+    Button _clickToLeftCapture;
+    Button _clickToRemotlyCapture;
+    Button _clickToStraightCapture;
+    private OnFragmentInteractionListener mListener;
+
+    public WelcomeScreenFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_screen, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        _clickToSimpleCapture = null;
+        _clickToRightCapture = null;
+        _clickToLeftCapture = null;
+        _clickToRemotlyCapture = null;
+        _clickToStraightCapture = null;
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {}
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)     {
+        //Initialize parameters
+        _clickToSimpleCapture = view.findViewById(R.id.simpleCaptureButton);
+        _clickToRightCapture = view.findViewById(R.id.rightCaptureButton);
+        _clickToLeftCapture = view.findViewById(R.id.leftCaptureButton);
+        _clickToRemotlyCapture = view.findViewById(R.id.remotlyCaptureButton);
+        _clickToStraightCapture = view.findViewById(R.id.straightCaptureButton);
+        AddListenerToSimpleCaptureButton();
+        AddListenerToRightCaptureButton();
+        AddListenerToLeftCaptureButton();
+        AddListenerToRemotlyCaptureButton();
+        AddListenerToStraightCaptureButton();
+    }
+
+    public void AddListenerToSimpleCaptureButton() {
+        _clickToSimpleCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    mListener.StartCameraFragment();
+                } catch (Exception e) {
+                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void AddListenerToRightCaptureButton() {
+        _clickToRightCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    mListener.StartCameraFragment();
+                } catch (Exception e) {
+                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void AddListenerToLeftCaptureButton() {
+        _clickToLeftCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    mListener.StartCameraFragment();
+                } catch (Exception e) {
+                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void AddListenerToRemotlyCaptureButton() {
+        _clickToRemotlyCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    mListener.StartCameraFragment();
+                } catch (Exception e) {
+                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void AddListenerToStraightCaptureButton() {
+        _clickToStraightCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    mListener.StartCameraFragment();
+                } catch (Exception e) {
+                    BillsLog.Log(Tag, LogLevel.Error, "StackTrace: " + e.getStackTrace() + "\nException Message: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void StartCameraFragment();
+    }
+}
