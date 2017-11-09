8fcca2ae49430ca3d43516ef8dfd6a91f8fd2d2d
Avraham, Aviel <avielas@gmail.com>
2017-11-09 20:45:50 +0200

    remove unusable code from FirebaseLogger

--- Bills/app/src/main/java/com/bills/bills/firebase/FirebaseLogger.java
+++ Bills/app/src/main/java/com/bills/bills/firebase/FirebaseLogger.java
@@ -24,20 +24,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class FirebaseLogger implements ILogger {
 
     private static String Tag = FirebaseLogger.class.getName();
+    private String mNow;
     private String mUserUid;
     private DatabaseReference mMyFirebaseLogReference;
     private DatabaseReference mFirebaseLogReferenceApp;
-    private String mNow;
-
     private long mRowCount;
-    private AtomicInteger mCurEntryCount = new AtomicInteger(0);
-
-    private AtomicBoolean mTransactionInProgress = new AtomicBoolean(false);
-
-    private AtomicBoolean mTerminateOnFinish = new AtomicBoolean(false);
     private Object mLock = new Object();
-
-    private ConcurrentLinkedQueue<LogEntry> mLogEntries = new ConcurrentLinkedQueue<>();
 
     public FirebaseLogger(String userUid, String firebaseDBCurrUserReference, String firebaseDBMainUserReference){
         mUserUid = userUid;
@@ -119,67 +111,4 @@ public class FirebaseLogger implements ILogger {
             });
         }
     }
-
-    private class LogEntry{
-        public final LogLevel mlogLevel;
-        public final String mMessage;
-
-        public LogEntry(LogLevel logLevel, String message){
-            mlogLevel = logLevel;
-            mMessage = message;
-        }
-    }
-
-//    private Thread GetWorkerThread(){
-//        return new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                ConcurrentLinkedQueue<LogEntry> logEntries = mLogEntries;
-//                AtomicBoolean terminateOnFinish = mTerminateOnFinish;
-//
-//                while(true){
-//                    if(logEntries.isEmpty()){
-//                        if(terminateOnFinish.get()){
-//                            return;
-//                        }
-//
-//                        try {
-//                            synchronized (this) {
-//                                this.wait(500);
-//                            }
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }else {
-//                        if (mTransactionInProgress.get()) {
-//                            continue;
-//                        }
-//
-//                        mTransactionInProgress.set(true);
-//                        final LogEntry logEntry = logEntries.poll();
-//                        mFirebaseLogReferenceApp.runTransaction(new Transaction.Handler() {
-//                            @Override
-//                            public Transaction.Result doTransaction(MutableData mutableData) {
-//                                if (mutableData == null || mutableData.getKey() == null) {
-//                                    return Transaction.abort();
-//                                }
-//                                mutableData.child(Integer.toString(mCurEntryCount.incrementAndGet())).
-//                                        setValue(logEntry.mlogLevel + ":" + logEntry.mMessage);
-//                                return Transaction.success(mutableData);
-//                            }
-//
-//                            @Override
-//                            public void onComplete(DatabaseError databaseError, boolean commited, DataSnapshot dataSnapshot) {
-//                                mTransactionInProgress.set(false);
-//                                synchronized (this) {
-//                                    this.notifyAll();
-//                                }
-//                            }
-//                        });
-//
-//                    }
-//                }
-//            }
-//        });
-//    }
 }
