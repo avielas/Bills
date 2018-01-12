ccf6a66d10dfa8c7516623b156e356259c28d138
Aviel Avraham <avielas@gmail.com>
2018-01-12 05:55:58 +0200

    request both, camera and external storage, permissions together

--- Bills/billslib/src/main/java/com/bills/billslib/Core/MainActivityBase.java
+++ Bills/billslib/src/main/java/com/bills/billslib/Core/MainActivityBase.java
@@ -1,7 +1,6 @@
 package com.bills.billslib.Core;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -10,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.bills.billslib.Contracts.PreparingEnvironmentUtil;
-import com.bills.billslib.Utilities.GMailSender;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
@@ -19,7 +17,9 @@ import pub.devrel.easypermissions.EasyPermissions;
  */
 
 public class MainActivityBase extends AppCompatActivity {
-    private static final int REQUEST_CAMERA_PERMISSION = 101;
+    private static final int REQUEST_CAMERA_AND_EXTERNAL_STORAGE_PERMISSIONS = 101;
+    private static final int REQUEST_CAMERA_PERMISSION = 102;
+    private static final int REQUEST_EXTERNAL_STORAGE_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +27,41 @@ public class MainActivityBase extends AppCompatActivity {
 
         //first visit of on create
         if(savedInstanceState == null){
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)                != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
                 // Should we show an explanation?
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                    Toast.makeText(this, "Camera access is required.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Camera and storage access is required!", Toast.LENGTH_SHORT).show();
 
                 } else {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
-                            REQUEST_CAMERA_PERMISSION);
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CAMERA_AND_EXTERNAL_STORAGE_PERMISSIONS);
                 }
 
-            }else if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
-
+            }else if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 // Should we show an explanation?
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE_PERMISSION);
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
 
                     // Show an explanation to the user *asynchronously* -- don't block
                     // this thread waiting for the user's response! After the user
                     // sees the explanation, try again to request the permission.
-
                 } else {
-
                     // No explanation needed, we can request the permission.
-
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                            1);
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
 
                     // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                     // app-defined int constant. The callback method gets the
@@ -68,7 +71,7 @@ public class MainActivityBase extends AppCompatActivity {
             PreparingEnvironmentUtil.PrepareTesseract(this);
         }
     }
-//
+
 //    @Override
 //    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 //        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
