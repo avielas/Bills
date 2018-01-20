127c59caa369b17a61d8a9022c60a42c2b137b15
Aviel Avraham <avielas@gmail.com>
2018-01-20 12:35:54 +0200

    use just GOOGLE provider when sign-in first time

--- Bills/app/src/main/java/com/bills/bills/BillsMainActivity.java
+++ Bills/app/src/main/java/com/bills/bills/BillsMainActivity.java
@@ -107,9 +107,7 @@ public class BillsMainActivity extends MainActivityBase implements
                                     .createSignInIntentBuilder()
                                     .setIsSmartLockEnabled(false)
                                     .setAvailableProviders(
-                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                                    new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
-                                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
                                     .build(),
                             RC_SIGN_IN);
                 }
