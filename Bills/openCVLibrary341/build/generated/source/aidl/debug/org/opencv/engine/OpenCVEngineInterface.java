6fccfc6257cebaa2ee697cf3bc87f5d4c84deb23
avielas@gmail.com <avielas@gmail.com>
2019-01-09 23:04:27 +0200

    upgrade build gradle from 3.1.2 to 3.1.4

--- Bills/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ Bills/openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/michaelvalershtein/Projects/BillSplitProject/Bills/bills/Bills/openCVLibrary341/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/aviel/Desktop/bills/Bills/openCVLibrary341/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
